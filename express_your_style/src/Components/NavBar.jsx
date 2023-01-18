b8e277c5d9abbfa8abc322eb313e3c14a4864fcd
TheSurajMore <surajmbusiness@gmail.com>
2023-01-18 20:42:51 +0530

    Day 2

--- express_your_style/src/Components/NavBar.jsx
+++ express_your_style/src/Components/NavBar.jsx
@@ -17,8 +17,11 @@ import {
   useColorMode,
   Center,
   Image,
+  IconButton,
+  HStack,
+
 } from '@chakra-ui/react';
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { MoonIcon, SunIcon, Search2Icon, CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
 
 const NavLink = (/*{ children }: { children: ReactNode }*/) => (
   <Link
@@ -41,14 +44,14 @@ export default function NavBar() {
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>Logo</Box>
-          <Image src='' alt='Logo'/>
+          <Box>Free Shipping on Orders $50+Details</Box>
+          <Image src='Suraj More Logo.jpg' alt='Express Your Style' width={130}/>
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
               <Button onClick={toggleColorMode}>
                 {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
               </Button>
-
+              <Search2Icon alignSelf={'center'} />
               <Menu>
                 <MenuButton
                   as={Button}
@@ -84,6 +87,114 @@ export default function NavBar() {
           </Flex>
         </Flex>
       </Box>
+    </>
+  );
+}
+
+// import { ReactNode } from 'react';
+// import {
+//   Box,
+//   Flex,
+//   Avatar,
+//   HStack,
+//   Link,
+//   IconButton,
+//   Button,
+//   Menu,
+//   MenuButton,
+//   MenuList,
+//   MenuItem,
+//   MenuDivider,
+//   useDisclosure,
+//   useColorModeValue,
+//   Stack,
+// } from '@chakra-ui/react';
+// import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const Links = ['WOMEN','MEN','JEANS', 'NEW', 'SALE', 'STYLE TRIAL RENTAL','COMMUNITY','LABBLS WE LOVE'];
+
+const Nav = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+export function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue(/*'gray.100', 'gray.900'*/)} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            {/* <Box>Logo</Box> */}
+            <HStack
+             as={'nav'}
+              spacing={4}
+              fontWeight={'bold'}
+              display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+              {
+                Links.map((el)=>(
+                  <Nav key={el}>{el}</Nav>
+                  ))
+              }
+            </HStack>
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Menu>
+              {/* <MenuButton
+                as={Button}
+                rounded={'full'}
+                variant={'link'}
+                cursor={'pointer'}
+                minW={0}>
+                <Avatar
+                  size={'sm'}
+                  src={
+                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                  }
+                />
+              </MenuButton> */}
+              <MenuList>
+                <MenuItem>Link 1</MenuItem>
+                <MenuItem>Link 2</MenuItem>
+                <MenuDivider />
+                <MenuItem>Link 3</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      {/* <Box p={4}>Main Content Here</Box> */}
     </>
   );
 }
