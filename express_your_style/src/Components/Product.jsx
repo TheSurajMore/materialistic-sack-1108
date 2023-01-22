3d9d73d9dfe80290699c8120502e172b52066335
TheSurajMore <surajmbusiness@gmail.com>
2023-01-22 09:39:22 +0530

    Buy Now

--- express_your_style/src/Components/Product.jsx
+++ express_your_style/src/Components/Product.jsx
@@ -1,8 +1,9 @@
-import { Box, Center, useColorModeValue, Heading, Text, Stack, Image,} from '@chakra-ui/react';
+import { Box, Center, useColorModeValue, Heading, Text, Stack, Image, Button,} from '@chakra-ui/react';
+import Buy from './BuyAlert';
   
 //   const IMAGE =
 //     'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
-  
+
   export default function Product(props) {
     const {title, rating_rate, rating_count, price, description, category, image } = props
     return (
@@ -54,16 +55,20 @@ import { Box, Center, useColorModeValue, Heading, Text, Stack, Image,} from '@ch
             <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
                {title}
             </Heading>
-            <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-             Rating: {rating_rate}({rating_count})
-            </Heading>
-            <Stack direction={'row'} align={'center'}>
-              <Text fontWeight={800} fontSize={'xl'}>
+            <Text fontWeight={800} fontSize={'xl'}>
                 ${price}
               </Text>
               <Text textDecoration={'line-through'} color={'gray.600'}>
                 $1999
               </Text>
+            <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+             Rating: {rating_rate}({rating_count})
+            </Heading>
+            <Stack direction='row' spacing={4}>
+              <Button colorScheme='pink' variant='solid' onClick={Buy}> Buy Now </Button>
+              {/* <Button colorScheme='blue' variant='outline'>Add to Cart</Button> */}
+            </Stack>
+            <Stack direction={'row'} align={'center'}>
               <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
               {description}
               </Text>
